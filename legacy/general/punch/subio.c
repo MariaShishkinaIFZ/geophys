af0bf6bed934f17a1171a6a977a10d73b221cfc3
Artem Gulyaev <gulyaev.artem.a@gmail.com>
2021-05-06 01:28:00 +0200

    [Legacy] added groomed apps
    * mspk
    * punch - forward task

---
+++ legacy/general/punch/subio.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "subio.h"
+
+FILE *file_open(const char *fname, const char *mode)
+{
+  FILE *f;
+  char s[80];
+  if (!(f=fopen(fname,mode)))
+  {
+	 sprintf(s,"Can't open file %s",fname);
+	 perror(s);
+	 printf("Program terminated.\n");
+	 exit(EXIT_FAILURE);
+  }
+  return f;
+}
+
+int file_close(FILE *f)
+{
+  return fclose(f);
+}
+
+void errquit(const char *s)
+{
+  puts(s); exit(EXIT_FAILURE);
+}
